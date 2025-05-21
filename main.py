fc5eca5698944dbb2bdb4ff9549a35f45c2e57bc
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2025-05-21 09:05:33 -0700

    updated ready print to be a messagebox

--- main.py
+++ main.py
@@ -78,6 +78,8 @@ def audio_thread():
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
     stream.start_stream()
+    # print("Say something!")
+    messagebox.showinfo("Ready", "Start speaking!")
     while True:
         data = stream.read(4000, exception_on_overflow=False)
         if recognizer.AcceptWaveform(data):
